7a04246cba0f443e7a5efd8bc83e8113a0222610
Christiansly <abiode23@gmail.com>
2022-02-09 01:39:19 +0100

    Generating PDF Files

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,7 @@ const Product = require("../models/product");
 const Order = require("../models/order");
 const path = require("path");
 const fs = require("fs");
+const PDFKit = require("pdfkit")
 exports.getProducts = (req, res) => {
   Product.find()
     .then((products) => {
@@ -161,17 +162,40 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = "invoice-" + orderId + ".pdf";
       const invoicePath = path.join("data", "invoices", invoiceName);
-      fs.readFile(invoicePath, (err, data) => {
-        if (err) {
-          return next(err);
-        }
-        res.setHeader("Content-Type", "application/pdf");
-        res.setHeader(
-          "Content-Disposition",
-          'inline; filename="' + invoiceName + '"'
-        );
-        res.send(data);
-      });
+
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) {
+      //     return next(err);
+      //   }
+      //   res.setHeader("Content-Type", "application/pdf");
+      //   res.setHeader(
+      //     "Content-Disposition",
+      //     'inline; filename="' + invoiceName + '"'
+      //   );
+      //   res.send(data);
+      // });
+
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        'inline; filename="' + invoiceName + '"'
+      );
+      const pdfDoc = new PDFKit()
+      pdfDoc.pipe(fs.createWriteStream(invoicePath))
+      pdfDoc.pipe(res)
+      pdfDoc.fontSize(26).text('Invoice', {
+        underline: true
+      })
+      pdfDoc.text('--------------------------------------------')
+      let totalPrice = 0
+      order.products.forEach(prod => {
+        totalPrice += prod.quantity * prod.product.price
+        pdfDoc.fontSize(14).text(prod.product.title + " - " + prod.quantity + " x " + prod.product.price)
+        pdfDoc.text("Total Price: $" + totalPrice)
+      })
+      pdfDoc.end()
+      // file.pipe(res)
     })
-    .catch();
+    .catch(err => next(err));
 };
